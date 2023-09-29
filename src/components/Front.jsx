a7383e2b4a1a1cb346eea88ec3247e20fab48242
Alok031 <iamaalokmishra@gmail.com>
2023-09-29 12:13:24 +0530

    main commit

---
+++ src/components/Front.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './Front.css'
+
+const Front = () => {
+  const [inputN, setInputN] = useState("")
+  const [items, setItems] = useState([])
+  const handleChange = (event) =>{
+    setInputN(event.target.value)
+  }
+  const handleClick = () =>{
+    setItems((prevItem)=>{
+         return [...prevItem,inputN];
+    })
+    setInputN("")
+  }
+  // setItems("");
+  return (
+    <>
+      <div className="container">
+        <div className="contain">
+        <br />
+          <h1>TodoList</h1>
+          <br />
+          <input type="text"  onChange={handleChange} value={inputN}/>
+          <button className="btn" onClick={handleClick}>+</button>
+          
+          <ul className='myUL'>
+            {items.map((itemVal)=>{
+              return <li>{itemVal}</li>;  
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Front
